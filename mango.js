a7d4c81ecfc1cdabd67b67da38038e4590b915d9
AdishKatwal <70854748+AdishKatwal@users.noreply.github.com>
2020-10-24 22:07:53 -0400

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,8 @@
+class Mango{
+    constructor(x,y){
+        this.x = Math.round(random(500,400));
+        this.y = Math.round(random(500,400));
+        this.image = loadImage("Plucking mangoes/mango.png");
+    }
+}
+
